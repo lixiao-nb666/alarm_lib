f7c0357be816a41cfd1b609708c74eb2537bf1e3
lixiao <lixiao@nb-666.com>
2023-06-07 10:44:21 +0800

    修改代碼

--- mylibrary/src/main/java/com/newbee/alarm_lib/manager/TaoziTimeClockManager.java
+++ mylibrary/src/main/java/com/newbee/alarm_lib/manager/TaoziTimeClockManager.java
@@ -1,9 +1,20 @@
 package com.newbee.alarm_lib.manager;
 
+import com.newbee.alarm_lib.bean.TaoziTimeBean;
+import com.newbee.alarm_lib.util.listen_time.ListenAlarmEventUtil;
+import com.newbee.alarm_lib.util.listen_time.ListenBetweenTimeEventUtil;
+import com.newbee.alarm_lib.util.listen_time.ListenNeedTimeMustDoEventUtil;
+import com.newbee.alarm_lib.util.listen_time.ListenTimeClockEventUtil;
+
 public class TaoziTimeClockManager {
 
 
     private static TaoziTimeClockManager manager;
+    private ListenTimeClockEventUtil listenTimeClockEventUtil=new ListenTimeClockEventUtil();
+    private ListenAlarmEventUtil listenAlarmEventUtil=new ListenAlarmEventUtil();
+    private ListenBetweenTimeEventUtil listenBetweenTimeEventUtil=new ListenBetweenTimeEventUtil();
+    private ListenNeedTimeMustDoEventUtil listenNeedTimeMustDoEventUtil=new ListenNeedTimeMustDoEventUtil();
+
     private TaoziTimeClockManager(){}
 
     public static TaoziTimeClockManager getInstance(){
@@ -15,6 +26,21 @@ public class TaoziTimeClockManager {
             }
         }
         return manager;
+    }
+
+    public void getSystemTime(TaoziTimeBean taoziTimeBean){
+        listenTimeClockEventUtil.getTimeSendListen(taoziTimeBean);
+        listenAlarmEventUtil.getTimeSendListen(taoziTimeBean);
+        listenBetweenTimeEventUtil.getTimeSendListen(taoziTimeBean);
+        listenNeedTimeMustDoEventUtil.getTimeSendListen(taoziTimeBean);
+    }
+
+
+    public void close(){
+        listenAlarmEventUtil.close();
+        listenBetweenTimeEventUtil.close();
+        listenNeedTimeMustDoEventUtil.close();
+        manager=null;
     }
 
 
